a55fc8ca4af592a2a233f750fb68966908a76def
Inezseavey13 <league@Mac-mini-4.local>
2017-11-30 17:38:07 -0800

    badgers

--- src/Badgers.java
+++ src/Badgers.java
@@ -1,24 +1,15 @@
 
 public class Badgers {
 	public static void main(String[] args) {
-		for (int i = 1; i < 11; i++) {
-			System.out.print("badger,");
+		for (int i = 1; i < 4; i++) {
+			System.out.println();
+			for (int j = 1; j < 11; j++) {
+				System.out.print("badger,");
+			}
+			System.out.println();
+			for (int k = 1; k < 3; k++) {
+				System.out.print("mushroom, ");
+			}
 		}
-		for (int i = 1; i < 3; i++) {
-			System.out.println("mushroom,");
-		}
-		for (int i = 1; i < 11; i++) {
-			System.out.print("badger,");
-		}
-		for (int i = 1; i < 3; i++) {
-			System.out.println("mushroom,");
-
-		}
-		for (int i = 1; i < 11; i++) {
-			System.out.print("badger,");
-		}
-		for (int i = 1; i < 11; i++) {
-	
-		}
-}
+	}
 }
